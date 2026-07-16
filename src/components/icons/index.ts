bfd5f91e1941dfa73e529645c44a377a063dee0c
alex <alexpumari0@gmail.com>
2026-07-16 12:25:58 -0300

    feat: add help and theme icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,5 @@ export * from "./home-icon.js";
 export * from "./mail-icon.js";
 export * from "./portfolio-icon.js";
 export * from "./profile-icon.js";
+export * from "./theme-icon.js";
+export * from "./help-icon.js";
---
+++ src/components/icons/theme-icon.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+import type{ IconProps } from "./icon-props.js";
+
+export const ThemeIcon: FC<IconProps> = (props) => {
+  return (
+    <svg viewBox="0 0 16 16" fill="none" {...props}>
+      <path fill="currentColor" fillRule="evenodd" d="M8 0a8 8 0 100 16A8 8 0 008 0zm0 1.5v13a6.5 6.5 0 100-13z" clipRule="evenodd"/>
+    </svg>
+  );
+}
