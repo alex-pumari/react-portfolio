52ef57218f9f8eff36c6bebe19f63a39fb67c954
alex <alexpumari0@gmail.com>
2026-08-17 21:07:49 -0300

    fix: improve fullscreen icon

--- src/components/icons/fullscreen-icon.tsx
+++ src/components/icons/fullscreen-icon.tsx
@@ -3,7 +3,7 @@ import type{ IconProps } from "./icon-props.js";
 
 export const FullscreenIcon: FC<IconProps> = (props) => {
   return (
-    <svg aria-hidden="true" viewBox="0 0 20 20" {...props}>
+    <svg aria-hidden="true" viewBox="0 0 20 20" shapeRendering="crispEdges" {...props}>
       <path
         fill="currentColor"
         d="M3 3H8V5.5H5.5V8H3V3ZM12 3H17V8H14.5V5.5H12V3ZM17 12V17H12V14.5H14.5V12H17ZM3 12H5.5V14.5H8V17H3V12Z"
