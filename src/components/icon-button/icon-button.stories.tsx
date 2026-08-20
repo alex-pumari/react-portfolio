6fbecfce7dedc3f52726fa3facf20f010b90fb7c
alex <alexpumari0@gmail.com>
2026-08-20 19:06:32 -0300

    feat: create icon button component

---
+++ src/components/icon-button/icon-button.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { IconButton } from "./icon-button.js";
+import { TrashIcon } from "../button/button-icons.js";
+
+const meta: Meta<typeof IconButton> = {
+  title: "Components/Icon Button",
+  component: IconButton,
+  tags: ["autodocs"],
+
+  argTypes: {
+    loading: {
+      control: "boolean",
+    },
+
+    disabled: {
+      control: "boolean",
+    },
+
+    onClick: {
+      action: "clicked",
+    },
+  },
+
+  args: {
+    icon: <TrashIcon />,
+    loading: false,
+    disabled: false,
+    title: "Delete",
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof IconButton>;
+
+export const Playground: Story = {};
+
+export const Loading: Story = {
+  args: {
+    loading: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
---
+++ src/components/icon-button/icon-button.tsx
@@ -0,0 +1,28 @@
+import type { ReactElement, FC } from "react";
+import type { ButtonProps } from "../button/button.js";
+import type { IconProps } from "../icons/icon-props.js";
+import { cloneElement } from "react"
+import { Button } from "../button/button.js";
+import { joinClasses } from "../../logic/join-classes.js";
+import "./icon-button.scss";
+
+interface IconButtonProps extends Omit<ButtonProps, "variant"> {
+  icon: ReactElement<IconProps>;
+  title: string
+}
+
+export const IconButton: FC<IconButtonProps> = ({ className, title, icon, ...props }) => {
+  const styledIcon = cloneElement(icon, {
+    className: joinClasses("icon-button__icon", icon.props.className),
+  });
+
+  return (
+    <Button
+      {...props}
+      icon={styledIcon}
+      title={title}
+      variant="outline"
+      className={joinClasses("icon-button", className)}
+    ></Button>
+  );
+};
