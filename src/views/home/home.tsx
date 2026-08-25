70d2ee7464ac1bdbd7a7ab0700511239d8046a27
alex <alexpumari0@gmail.com>
2026-08-25 15:16:00 -0300

    refactor: use panel component in home view

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react";
-import { WindowCard } from "../../components/window-card/window-card.js";
+import { Panel } from "../../components/panel/panel.js";
 import { Button } from "../../components/button/button.js";
+import { WindowCard } from "../../components/window-card/window-card.js";
 import profileImgPath from "../../assets/images/profile.webp";
 import "./home.scss";
 
@@ -10,7 +11,7 @@ export const Home: FC = () => {
       <div className="home-view__content">
         <div className="home-view__info">
           <h1 className="home-view__title">ALEX PUMARI DIAZ</h1>
-          <p className="home-view__subtitle">DESARROLLADOR FULL STACK & ANALISTA DE SISTEMAS</p>
+          <p className="home-view__subtitle">ANALISTA DE SISTEMAS & DESARROLLADOR FULL STACK</p>
           <p className="home-view__description">
             Creo que el buen software nace de la simplicidad. Por eso diseño y desarrollo soluciones pensadas para ser rápidas, escalables y fáciles de mantener.
           </p>
@@ -19,9 +20,9 @@ export const Home: FC = () => {
             <Button variant="outline" size="lg">CONTACTAME</Button>
           </div>
         </div>
-        <div className="home-view__image-container">
+        <Panel className="home-view__image-container">
           <img className="home-view__image" src={profileImgPath} alt="Foto de perfil" />
-        </div>
+        </Panel>
       </div>
     </WindowCard>
   );
